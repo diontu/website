c3115f13e59ab803a41f9f420802a032aff279f2
diontugit <diontugit@gmail.com>
2024-01-13 21:22:27 -0500

    minor: edited path resolution, gitignore and imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import routerPaths from './router/router.tsx'
+import routerPaths from 'router/router'
 import './index.css'
 
 const router = createBrowserRouter(routerPaths)
