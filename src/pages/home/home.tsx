58d12d5cc5af06e68f8b35082440a61caec4d1cd
diontugit <diontugit@gmail.com>
2024-01-16 13:00:58 -0500

    minor: modify with existing projects

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -45,14 +45,15 @@ const Home = (): JSX.Element => {
                     </p>
                 </div>
             </CardSection>
-            {/* todo: modify this with my existing projects */}
-            <CardSection id="projects" title="Projects">
+            <CardSection
+                id="projects"
+                title="Projects"
+                description="These are some of the personal projects I have worked
+                        on"
+            >
                 <div className="text-left">
-                    <p className="mb-4">
-                        Introduction on me working on projects
-                    </p>
                     <section className="mt-16 text-center">
-                        <div className="grid gap-6 lg:grid-cols-3 xl:gap-x-12">
+                        <div className="grid gap-6 lg:grid-cols-2 xl:gap-x-12">
                             <div className="mb-6 lg:mb-0">
                                 <div className="relative block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
                                     <div className="flex">
@@ -62,73 +63,24 @@ const Home = (): JSX.Element => {
                                             data-te-ripple-color="light"
                                         >
                                             <img
-                                                src="https://mdbcdn.b-cdn.net/img/new/standard/city/002.webp"
-                                                className="w-full"
+                                                src="/website.png"
+                                                className="w-full h-[300px] object-cover"
                                             />
-                                            <a href="#!">
-                                                <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
-                                            </a>
                                         </div>
                                     </div>
                                     <div className="p-6">
                                         <h5 className="mb-4 text-lg font-bold">
-                                            White city
+                                            This Website
                                         </h5>
                                         <p className="mb-6">
-                                            Ut pretium ultricies dignissim. Sed
-                                            sit amet mi eget urna placerat
-                                            vulputate. Ut vulputate est non quam
-                                            dignissim elementum. Donec a
-                                            ullamcorper diam.
+                                            This website is built using many of
+                                            the more recent development tools
+                                            such as Vite and Tailwind CSS. I
+                                            will constantly be updating this
+                                            website with new development tools I
+                                            find to ultimately my increase
+                                            productivity.
                                         </p>
-                                        <a
-                                            href="#!"
-                                            data-te-ripple-init
-                                            data-te-ripple-color="light"
-                                            className="inline-block rounded-full bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-                                        >
-                                            Read more
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="mb-6 lg:mb-0">
-                                <div className="relative block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-                                    <div className="flex">
-                                        <div
-                                            className="relative mx-4 -mt-4 w-full overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]"
-                                            data-te-ripple-init
-                                            data-te-ripple-color="light"
-                                        >
-                                            <img
-                                                src="https://mdbcdn.b-cdn.net/img/new/standard/people/066.webp"
-                                                className="w-full"
-                                            />
-                                            <a href="#!">
-                                                <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <div className="p-6">
-                                        <h5 className="mb-4 text-lg font-bold">
-                                            A lonely bench
-                                        </h5>
-                                        <p className="mb-6">
-                                            Suspendisse in volutpat massa. Nulla
-                                            facilisi. Sed aliquet diam orci, nec
-                                            ornare metus semper sed. Integer
-                                            volutpat ornare erat sit amet
-                                            rutrum.
-                                        </p>
-                                        <a
-                                            href="#!"
-                                            data-te-ripple-init
-                                            data-te-ripple-color="light"
-                                            className="inline-block rounded-full bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-                                        >
-                                            Read more
-                                        </a>
                                     </div>
                                 </div>
                             </div>
@@ -142,33 +94,23 @@ const Home = (): JSX.Element => {
                                             data-te-ripple-color="light"
                                         >
                                             <img
-                                                src="https://mdbcdn.b-cdn.net/img/new/standard/people/191.webp"
-                                                className="w-full"
+                                                src="/youtube_channel.png"
+                                                className="w-full h-[300px] object-cover"
                                             />
-                                            <a href="#!">
-                                                <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
-                                            </a>
                                         </div>
                                     </div>
                                     <div className="p-6">
                                         <h5 className="mb-4 text-lg font-bold">
-                                            Happy snow
+                                            YouTube Channel
                                         </h5>
                                         <p className="mb-6">
-                                            Curabitur tristique, mi a mollis
-                                            sagittis, metus felis mattis arcu,
-                                            non vehicula nisl dui quis diam.
-                                            Mauris ut risus eget massa volutpat
-                                            feugiat. Donec.
+                                            I've started a YouTube channel to
+                                            showcase my journey in my career.
+                                            Any lessons I've learned is
+                                            essentially (or will be) imparted
+                                            into this channel to educate others
+                                            what it's like to be in this field.
                                         </p>
-                                        <a
-                                            href="#!"
-                                            data-te-ripple-init
-                                            data-te-ripple-color="light"
-                                            className="inline-block rounded-full bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-                                        >
-                                            Read more
-                                        </a>
                                     </div>
                                 </div>
                             </div>
