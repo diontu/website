3451545b1af651e4aa5b2a4cd884ea14bff6d657
diontugit <diontugit@gmail.com>
2024-01-16 23:52:42 -0500

    minor: added 404 error and replace router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,9 +1,19 @@
-import Home from 'pages/home/home'
-import Recipes from 'pages/recipes/recipes'
+import Error404 from '@/pages/404/Error404'
+import Home from '@/pages/home/home'
+import Recipes from '@/pages/recipes/recipes'
+import { Navigate } from 'react-router-dom'
 
 const routerPaths = [
     {
-        path: '',
+        path: '*',
+        element: <Navigate to="/404" replace />,
+    },
+    {
+        path: '/404',
+        element: <Error404 />,
+    },
+    {
+        path: '/',
         element: <Home />,
     },
     {
