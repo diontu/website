05a191abb51001e5bc5840893c20d7b954f4b177
diontugit <diontugit@gmail.com>
2024-01-17 23:45:13 -0500

    minor: edit recipes page with a recipes catalog

---
+++ src/pages/recipes/recipes-catalog.tsx
@@ -0,0 +1,183 @@
+const RecipesCatalog = (): JSX.Element => {
+    return (
+        <section className="mb-32">
+            <div className="grid gap-6 lg:grid-cols-3">
+                <div
+                    className="hover:scale-105 duration-500 relative overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-lg dark:shadow-black/20"
+                    data-te-ripple-init
+                    data-te-ripple-color="light"
+                >
+                    <img
+                        src="https://mdbcdn.b-cdn.net/img/new/standard/nature/051.jpg"
+                        className="w-full align-middle transition duration-300 ease-linear"
+                    />
+                    <a href="#!">
+                        <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsla(0,0%,0%,0.4)] bg-fixed">
+                            <div className="flex h-full items-end justify-start">
+                                <div className="m-6 text-white">
+                                    <h5 className="mb-3 text-lg font-bold">
+                                        I miss the sun
+                                    </h5>
+                                    <p>
+                                        <small>
+                                            Published <u>13.01.2022</u> by Anna
+                                            Maria Doe
+                                        </small>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed transition duration-300 ease-in-out hover:bg-[hsla(0,0%,99%,0.15)]"></div>
+                    </a>
+                </div>
+
+                <div
+                    className="hover:scale-105 duration-500 relative overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-lg dark:shadow-black/20"
+                    data-te-ripple-init
+                    data-te-ripple-color="light"
+                >
+                    <img
+                        src="https://mdbcdn.b-cdn.net/img/new/standard/nature/044.jpg"
+                        className="w-full align-middle transition duration-300 ease-linear"
+                    />
+                    <a href="#!">
+                        <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsla(0,0%,0%,0.4)] bg-fixed">
+                            <div className="flex h-full items-end justify-start">
+                                <div className="m-6 text-white">
+                                    <h5 className="mb-3 text-lg font-bold">
+                                        Adventure in the desert
+                                    </h5>
+                                    <p>
+                                        <small>
+                                            Published <u>12.01.2022</u> by Mark
+                                            Equel
+                                        </small>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed transition duration-300 ease-in-out hover:bg-[hsla(0,0%,99%,0.15)]"></div>
+                    </a>
+                </div>
+
+                <div
+                    className="hover:scale-105 duration-500 relative overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-lg dark:shadow-black/20"
+                    data-te-ripple-init
+                    data-te-ripple-color="light"
+                >
+                    <img
+                        src="https://mdbcdn.b-cdn.net/img/new/standard/nature/045.jpg"
+                        className="w-full align-middle transition duration-300 ease-linear"
+                    />
+                    <a href="#!">
+                        <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsla(0,0%,0%,0.4)] bg-fixed">
+                            <div className="flex h-full items-end justify-start">
+                                <div className="m-6 text-white">
+                                    <h5 className="mb-3 text-lg font-bold">
+                                        Lonely mountain
+                                    </h5>
+                                    <p>
+                                        <small>
+                                            Published <u>10.01.2022</u> by Bilbo
+                                            baggins
+                                        </small>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed transition duration-300 ease-in-out hover:bg-[hsla(0,0%,99%,0.15)]"></div>
+                    </a>
+                </div>
+
+                <div
+                    className="hover:scale-105 duration-500 relative overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-lg dark:shadow-black/20"
+                    data-te-ripple-init
+                    data-te-ripple-color="light"
+                >
+                    <img
+                        src="https://mdbcdn.b-cdn.net/img/new/standard/nature/047.jpg"
+                        className="w-full align-middle transition duration-300 ease-linear"
+                    />
+                    <a href="#!">
+                        <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsla(0,0%,0%,0.4)] bg-fixed">
+                            <div className="flex h-full items-end justify-start">
+                                <div className="m-6 text-white">
+                                    <h5 className="mb-3 text-lg font-bold">
+                                        Let's go!
+                                    </h5>
+                                    <p>
+                                        <small>
+                                            Published <u>09.01.2022</u> by
+                                            Halley Frank
+                                        </small>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed transition duration-300 ease-in-out hover:bg-[hsla(0,0%,99%,0.15)]"></div>
+                    </a>
+                </div>
+
+                <div
+                    className="hover:scale-105 duration-500 relative overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-lg dark:shadow-black/20"
+                    data-te-ripple-init
+                    data-te-ripple-color="light"
+                >
+                    <img
+                        src="https://mdbcdn.b-cdn.net/img/new/standard/nature/028.jpg"
+                        className="w-full align-middle transition duration-300 ease-linear"
+                    />
+                    <a href="#!">
+                        <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsla(0,0%,0%,0.4)] bg-fixed">
+                            <div className="flex h-full items-end justify-start">
+                                <div className="m-6 text-white">
+                                    <h5 className="mb-3 text-lg font-bold">
+                                        A hut in the mountains
+                                    </h5>
+                                    <p>
+                                        <small>
+                                            Published <u>07.01.2022</u> by David
+                                            Beak
+                                        </small>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed transition duration-300 ease-in-out hover:bg-[hsla(0,0%,99%,0.15)]"></div>
+                    </a>
+                </div>
+
+                <div
+                    className="hover:scale-105 duration-500 relative overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-lg dark:shadow-black/20"
+                    data-te-ripple-init
+                    data-te-ripple-color="light"
+                >
+                    <img
+                        src="https://mdbcdn.b-cdn.net/img/new/standard/nature/049.jpg"
+                        className="w-full align-middle transition duration-300 ease-linear"
+                    />
+                    <a href="#!">
+                        <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsla(0,0%,0%,0.4)] bg-fixed">
+                            <div className="flex h-full items-end justify-start">
+                                <div className="m-6 text-white">
+                                    <h5 className="mb-3 text-lg font-bold">
+                                        Beautiful waterfall
+                                    </h5>
+                                    <p>
+                                        <small>
+                                            Published <u>04.01.2022</u> by Joe
+                                            Svan
+                                        </small>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed transition duration-300 ease-in-out hover:bg-[hsla(0,0%,99%,0.15)]"></div>
+                    </a>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default RecipesCatalog
--- src/pages/recipes/recipes.tsx
+++ src/pages/recipes/recipes.tsx
@@ -1,7 +1,28 @@
 import BlankPage from '@/page-templates/blank-page/blank-page'
+import RecipesCatalog from '@/pages/recipes/recipes-catalog'
 
 const Recipes = (): JSX.Element => {
-    return <BlankPage>Recipes Page</BlankPage>
+    return (
+        <BlankPage>
+            <div className="container my-24 mx-auto md:px-6">
+                <h2 className="mb-12 text-center text-3xl font-bold">
+                    Latest Recipes
+                </h2>
+                <p>
+                    Welcome to my world of recipes! I'm a big fan of eating
+                    food, but I also like to cook it as well. I tend to forgot a
+                    lot of the recipes I've cooked quite often so that's why I
+                    made this page. My favourite cuisines to eat/cook is
+                    Vietnamese, but would love to try new recipes too! If you
+                    have a recipe that you love and would like to share, please
+                    don't hesitate to with me{' '}
+                    <a href="https://twitter.com/DionTu3">@DionTu3</a> and I
+                    will consider making it and adding to the recipes here!
+                </p>
+                <RecipesCatalog />
+            </div>
+        </BlankPage>
+    )
 }
 
 export default Recipes
