418766d9f4e62baf7c0c6a31d183cabdfdd2d350
diontugit <diontugit@gmail.com>
2024-01-20 22:20:30 -0500

    minor: added logo and favicon

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,23 +1,17 @@
-import { useNavigate } from 'react-router-dom'
 import Navigation, {
     NavMenu,
 } from '@/components/header/navigation-menu/navigation-menu'
-import reactLogo from '@/assets/react.svg'
 
 type HeaderProps = {
     nav: NavMenu
 }
 
 const Header = (props: HeaderProps): JSX.Element => {
-    const navigate = useNavigate()
     return (
         <div className="flex z-10 sticky top-0 w-full py-3 bg-white border-b-2 border-slate-150">
-            {/* TODO: replace with my own logo or initials */}
-            {/* <img
-                src={reactLogo}
-                className="h-12 w-12 cursor-pointer"
-                onClick={() => navigate('/')}
-            /> */}
+            <a href="/">
+                <img src="/logo.png" className="h-12 w-12 cursor-pointer" />
+            </a>
             <Navigation nav={props.nav} />
         </div>
     )
