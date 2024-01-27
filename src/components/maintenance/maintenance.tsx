800a1703551ce0bd51ebdcd1089ace44ae202f52
diontugit <diontugit@gmail.com>
2024-01-26 23:33:28 -0500

    minor: add maintenance pages

---
+++ src/components/maintenance/maintenance.tsx
@@ -0,0 +1,19 @@
+const Maintenance = (): JSX.Element => {
+    return (
+        <div className="flex flex-col justify-cente my-24">
+            <p className="text-2xl">
+                I'm just building this right now... come back again and I'll
+                have it ready for you!
+            </p>
+            <div className="flex justify-center">
+                <img
+                    src="/public/maintenance-panel.gif"
+                    alt="maintenance gif"
+                    className="md:w-7/12 md:h-[400px] object-cover"
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Maintenance
