882a3f2e5772b5da802adab35cfe7c954c9b9fa0
diontugit <diontugit@gmail.com>
2024-02-02 23:17:02 -0500

    minor: format recipe page

--- src/pages/recipes/recipe/recipe.tsx
+++ src/pages/recipes/recipe/recipe.tsx
@@ -5,6 +5,8 @@ import { ContentResponse, RecipeSchema } from '@/api/api'
 import { BLOCKS } from '@contentful/rich-text-types'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import { parseContentfulImageUrl } from '@/utils/utils'
+import Card from '@/components/card/card'
+import { Checkbox } from '@/components/ui/checkbox'
 
 // TODO: make the story an accordion, everything else should be fully visible
 // TODO: add columns to make this look nicer
@@ -39,17 +41,35 @@ const Recipe = (): JSX.Element => {
                                 recipe.fields.thumbnail.fields.file.url
                             )}
                             alt="recipe_pic"
-                            className="h-[400px] w-[400px]"
+                            className="max-h-[400px] w-[400px] object-cover"
                         />
                     </div>
                     {recipe.fields.description}
                     {/* TODO: create checkbox for ingredients? with cache that expires every 24 hours? */}
-                    {recipe.fields.ingredients}
+                    <div className="my-6">
+                        <Card title="Ingredients">
+                            {recipe.fields.ingredients.map((ingredient) => (
+                                <div className="flex align-center">
+                                    <Checkbox id={ingredient} />
+                                    <label
+                                        htmlFor={ingredient}
+                                        className="pl-3 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                    >
+                                        {ingredient}
+                                    </label>
+                                </div>
+                            ))}
+                        </Card>
+                    </div>
                     {/* cooking instructions */}
-                    {documentToReactComponents(
-                        recipe.fields.cookingSteps,
-                        getRenderOptions()
-                    )}
+                    <div className="my-6">
+                        <Card title="Cooking Steps">
+                            {documentToReactComponents(
+                                recipe.fields.cookingSteps,
+                                getRenderOptions()
+                            )}
+                        </Card>
+                    </div>
                 </div>
             )}
         </BlankPage>
